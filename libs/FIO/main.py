7d5a4d12f44708b592ab007560f2aed04b08f6ae
SuleimanMarcel <Marcel.Suleiman@external.drei.com>
2025-10-30 12:07:56 +0100

    move FIO into libs folder

--- libs/FIO/main.py
+++ libs/FIO/main.py
@@ -1,8 +1,11 @@
+from .__init__ import __version__
 
 
 def main():
     print("This is the FIO main module.")
     print("extra line")
+
+    print(__version__)
 
 
 if __name__ == '__main__':
---
+++ main.py
@@ -0,0 +1,4 @@
+from libs.FIO.main import main as fio_main
+
+if __name__ == '__main__':
+    fio_main()
